a2d862938bd86e3b872f047a40d44d1c22f0612b
nikopippuri <127618961+nikopippuri@users.noreply.github.com>
2024-09-29 20:29:18 +0300

    Buttons osion alustus ja koodin alkua

--- Koodit/SpedenSpelit/buttons.cpp
+++ Koodit/SpedenSpelit/buttons.cpp
@@ -1,17 +1,55 @@
 #include "buttons.h"
 
 
-
+volatile int pcint1PinLow = 0;  
 
 void initButtonsAndButtonInterrupts(void)
 {
   // See requirements for this function from buttons.h
+
+      // Asetetaan pinnit A0-A3 (14-17) käyttämään INPUT_PULLUP -tilaa
+    for (int i = A0; i <= A3; i++) {
+        pinMode(i, INPUT_PULLUP);
+    }
+
+    // Aktivoidaan PCINT1 (Portti C), muut portit disabloitu
+    PCICR |= (1 << PCIE1); // PCINT1 käytössä
+
+    // Enable PCINT14-17 (pinnit A0-A3)
+    PCMSK1 = B00001111; // 0x0F; // Enable pins A0, A1, A2, A3
+
+    Serial.begin(9600); // Sarjaviestintä alkaa nopeudella 9600 bps
+    interrupts(); // Keskeytykset enabloidaan
+
 }
 
-ISR(PCINT2_vect) {
+ISR(PCINT1_vect) {
+
+  for (int i = A0; i <= A3; i++) {
+        // Luetaan nykyinen tila
+        byte pinState = digitalRead(i);
+        
+        if (pinState == LOW) {
+            // Jos pinni on LOW, talletetaan se
+            pcint1PinLow = i;
+            break; // Lopetetaan, kun ensimmäinen LOW-pinni on löydetty
+        }
+
    /*
      Here you implement logic for handling
 	 interrupts from 2,3,4,5 pins for Game push buttons
 	 and for pin 6 for start Game push button.
    */
+}
+
+void mika_nappi_painettiin() {
+   // Tarkistetaan, onko tapahtunut keskeytys pinneistä A0-A3
+    if (pcint1PinLow != 0) {
+        Serial.print("PCINT1-väylästä keskeytys pinnistä HIGH to LOW = ");
+        Serial.println(pcint1PinLow);
+      
+      // LISÄÄ SAMALLA KYSEINEN LUKU PAINETUT_NAPPULAT[oikea_indeksi] taulukkoon??
+      
+        pcint1PinLow = 0; // Nollataan arvo, jotta voidaan käsitellä seuraava keskeytys
+    }
 }
