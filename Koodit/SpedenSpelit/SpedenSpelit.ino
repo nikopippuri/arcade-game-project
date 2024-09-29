c0dedcd668fc25750fe8a21133967e9e9634f4e9
IiroToivari <Iiro.Toivari@gmail.com>
2024-09-29 18:21:29 +0300

    Update SpedenSpelit.ino
    Raakile_1

--- Koodit/SpedenSpelit/SpedenSpelit.ino
+++ Koodit/SpedenSpelit/SpedenSpelit.ino
@@ -1,85 +1,72 @@
-#ifndef SPEDENSPELIT_H
-#define SPEDENSPELIT_H
-#include <arduino.h>
-#include <avr/io.h>
-#include <avr/interrupt.h>
+#include "display.h"
+#include "buttons.h"
+#include "leds.h"
+#include "SpedenSpelit.h"
 
-/*
-  initializeTimer() subroutine intializes Arduino Timer1 module to
-  give interrupts at rate 1Hz
+// Use these 2 volatile variables for communicating between
+// loop() function and interrupt handlers
+volatile int buttonNumber = -1;           // for buttons interrupt handler
+volatile bool newTimerInterrupt = false;  // for timer interrupt handler
+
+
+void setup()
+{
+  /*
+  initializeTimer1? 
+    Initialize here all modules
+    alusta kaikki moodulit tässä
+  */
+
+initializerTimer();  //Timer 1 alustus
+initButtonsAndButtonInterrupts();  //buttons.h tiedostosta
+
+
+}
+
+void loop()
+{
+  if(buttonNumber>=0)
+  {
+     // start the game if buttonNumber == 4
+     // check the game if 0<=buttonNumber<4
+     
+    
+  }
+
+  if(newTimerInterrupt == true)
+  {
+     // new random number must be generated
+     // and corresponding let must be activated
+  }
+}
+
+void initializeTimer(void)
+{
+	// see requirements for the function from SpedenSpelit.h
+}
+ISR(TIMER1_COMPA_vect)
+{
+  /*
+  Communicate to loop() that it's time to make new random number.
+  Increase timer interrupt rate after 10 interrupts.
+  */
   
-*/
-void initializeTimer(){
-   TCCR1A = 0;   //VGM 11 ja VGM10 bitit nollaksi
-   TCCR1B = 00001011;   // prescaler  
-   OCR1A  = 250000;    // -> keskeytys, kun saavuttaa arvon 250000 = vertailuarvo
-   TIMSK1 = 00000010; // enabloidaan vertailu keskeytykset
-  interrupts();  //enabloidaan globaalit keskeytykset
 }
 
 
-void initializeTimer();
-// Intoduce TIMER1_COMPA_vect Interrupt SeRvice (ISR) function for timer.
-ISR(TIMER1_COMPA_vect);  /* keskeytyspalvelia   ISR = interupServiceRoutine   tähän hypätään, kun timerilta tulee keskeytys --> ocr1A saavuttaa arvon 250000 
-Suorittaa alla olevan ohjelma ja palaaa suorittamaan missä olikin. keskeytys tulee ja uusi luku arvotaan -> ledi syttyy*/
-
-
-/*
-  initializeGame() subroutine is used to initialize all variables
-  needed to store random numbers and player button push data.
-  This function is called from startTheGame() function.
-  
-*/
-void initializeGame();
-
-string randomNumbers[100];  //Arvotut luvut     Arpominen tapahtuu Timer1:n keskeytyspalvelijassa
-string userNumbers[100]; //painetut napit   talletetaan näppäinten keskeytyspalvelijassa
-
-
-/*
-  checkGame() subroutine is used to check the status
-  of the Game after each player button press.
-  
-  If the latest player button press is wrong, the game stops
-  and if the latest press was right, game display is incremented
-  by 1.
-  
-  Parameters
-  byte lastButtonPress of the player 0 or 1 or 2 or 3
-  
-*/
-void checkGame(byte);  //ottaa parametrit "byte"
-
-   if(userNumbers != randomNumbers) {
-
-     stopTheGame();  //peli päättyy
-
-      } else {
-          showResults();  // lisataanpiste ja peli jatkuu
+void checkGame(byte nbrOfButtonPush)
+{
+	// see requirements for the function from SpedenSpelit.h
 }
 
 
-/*
-  void stopTheGame(void)
-  keskeyttaa pelin ja nayttää tuloksen -> resetointi 
-*/
-void stopTheGame() {
-    TCCR1B = 0;  //pysäyttää timerin laskennan
-    TIMSK1 &= ~(1 << OCR1A);   //shift rekister left = disabloi keskeytykset. 
-    showResults();  //Kutsuu funktiota missä näytetään lopputulos jne..
+void initializeGame()
+{
+	// see requirements for the function from SpedenSpelit.h
 }
 
-
-/*
-  startTheGame() subroutine calls InitializeGame()
-  function and enables Timer1 interrupts to start
-  the Game.
-*/
-void startTheGame() {
-
-   InitializeGame();
+void startTheGame()
+{
+   // see requirements for the function from SpedenSpelit.h
 }
 
-
-
-#endif
