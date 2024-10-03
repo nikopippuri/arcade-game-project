5b9c7967ab28dbc754e76aac8898a15bee98241a
nikopippuri <127618961+nikopippuri@users.noreply.github.com>
2024-10-03 20:24:12 +0300

    Update leds.h
    koodin korjaus

--- Koodit/SpedenSpelit/leds.h
+++ Koodit/SpedenSpelit/leds.h
@@ -1,36 +1,8 @@
-#include "leds.h"
+#include <arduino.h>
 
-
-void initializeLeds()
-{
-// see requirements for this function from leds.h
-}
-
-void setLed(byte ledNumber)
-{
-// see requirements for this function from leds.h
-
-}
-
-
-void clearAllLeds()
-{
-// see requirements for this function from leds.h
- 
-}
-
-void setAllLeds()
-{
-// see requirements for this function from leds.h
-}
-
-
-void show1()
-{
-// see requirements for this function from leds.h
-}
-
-void show2(int rounds)
-{
-// see requirements for this function from leds.h  
-}
+void initializeLeds();
+void setLed(byte ledNumber);
+void clearAllLeds();
+void setAllLeds();
+void show1();
+void show2(int rounds);
